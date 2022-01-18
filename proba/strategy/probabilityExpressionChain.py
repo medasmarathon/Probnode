e85891d4ec376df1f6f25bfdf29d79abb3ad953a
Duc Luong <vinhduc91@outlook.com>
2022-01-18 09:16:30 +0700

    edit repr of expression chain

--- proba/strategy/probabilityExpressionChain.py
+++ proba/strategy/probabilityExpressionChain.py
@@ -16,4 +16,4 @@ class ProbabilityExpressionChain:
     return self
 
   def __repr__(self) -> str:
-    return f"{self.__class__.__name__}::{self.chain}"
+    return " ".join(repr(item) for item in self.chain)
