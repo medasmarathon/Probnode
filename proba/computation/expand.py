ec8599a8740d9c213ab43bae2b64a318b3623966
Duc Luong <vinhduc91@outlook.com>
2022-02-04 17:43:33 +0700

    add expanding functions

--- proba/computation/expand.py
+++ proba/computation/expand.py
@@ -1,5 +1,42 @@
-from proba.computation.chain import Node
+from proba.computation.chain import AdditiveInverseNode, ChainNode, DerivedNode, Node, ReciprocalNode
+from proba.computation.probabilityLogic import apply_probability_logic
+from proba.probability import BaseProbabilityExpression
 
 
-def expand(node: Node):
+def expand(node: Node, exhausting: bool = False):
+  if issubclass(type(node), ChainNode):
+    return expand_chain_node(node, exhausting)
+  if issubclass(type(node), DerivedNode):
+    return expand_derived_node(node, exhausting)
+  else:
+    return expand_pure_node(node, exhausting)
+
+
+def expand_pure_node(node: Node, exhausting: bool = False):
+  node.exp = expand_probability_exp(node.exp)
   return node
+
+
+def expand_derived_node(node: DerivedNode, exhausting: bool = False):
+  if type(node) is AdditiveInverseNode:
+    expanded_base = expand_pure_node(node.base, exhausting)
+    return AdditiveInverseNode.from_node(expanded_base)
+  if type(node) is ReciprocalNode:
+    expanded_base = expand_pure_node(node.base, exhausting)
+    return ReciprocalNode.from_node(expanded_base)
+  return node
+
+
+def expand_chain_node(chain_node: ChainNode, exhausting: bool = False):
+  for idx, node in enumerate(chain_node.args):
+    if issubclass(type(node), ChainNode):
+      chain_node.args[idx] = expand_derived_node(node)
+    elif issubclass(type(node), DerivedNode):
+      chain_node.args[idx] = expand_derived_node(node)
+    else:
+      chain_node.args[idx] = expand_pure_node(node)
+  return chain_node
+
+
+def expand_probability_exp(expression: BaseProbabilityExpression) -> BaseProbabilityExpression:
+  return apply_probability_logic(expression)
---
+++ proba/computation/probabilityLogic.py
@@ -0,0 +1,5 @@
+from proba.probability import BaseProbabilityExpression
+
+
+def apply_probability_logic(expression: BaseProbabilityExpression) -> BaseProbabilityExpression:
+  return expression
