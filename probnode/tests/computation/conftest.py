4783e8399682a41aaf1312639a96ab7859571744
Duc Luong <vinhduc91@outlook.com>
2022-02-10 23:40:50 +0700

    add test expand

--- probnode/tests/computation/conftest.py
+++ probnode/tests/computation/conftest.py
@@ -7,31 +7,35 @@ from probnode.probability.probability import SimpleProbabilityExpression
 
 
 @pytest.fixture(autouse=True)
-def simple_prob_expression(evt_name: str):
-  return P(Event(evt_name))
+def simple_prob_expression1():
+  return P(Event("Hot"))
 
 
 @pytest.fixture(autouse=True)
-def simple_invert_prob_expression(evt_name: str):
-  return simple_prob_expression(evt_name).invert()
+def simple_prob_expression2():
+  return P(Event("Snow"))
 
 
 @pytest.fixture(autouse=True)
-def or_prob_expression():
-  p1 = simple_prob_expression("Hot")
-  p2 = simple_prob_expression("Rain")
-  return P(p1 | p2)
+def simple_invert_prob_expression1(simple_prob_expression1: SimpleProbabilityExpression):
+  return simple_prob_expression1.invert()
 
 
 @pytest.fixture(autouse=True)
-def and_prob_expression():
-  p1 = simple_prob_expression("Cold")
-  p2 = simple_prob_expression("Snow")
-  return P(p1 & p2)
+def simple_invert_prob_expression2(simple_prob_expression2: SimpleProbabilityExpression):
+  return simple_prob_expression2.invert()
 
 
 @pytest.fixture(autouse=True)
-def conditional_prob_expression():
-  p1 = simple_prob_expression("Hot")
-  p2 = simple_prob_expression("Winter")
-  return P(p1 // p2)
+def or_prob_expression(simple_prob_expression1, simple_prob_expression2):
+  return P(simple_prob_expression1 | simple_prob_expression2)
+
+
+@pytest.fixture(autouse=True)
+def and_prob_expression(simple_prob_expression1, simple_prob_expression2):
+  return P(simple_prob_expression1 & simple_prob_expression2)
+
+
+@pytest.fixture(autouse=True)
+def conditional_prob_expression(simple_prob_expression1, simple_prob_expression2):
+  return P(simple_prob_expression1 // simple_prob_expression2)
--- probnode/tests/computation/test_expand.py
+++ probnode/tests/computation/test_expand.py
@@ -1,12 +1,47 @@
-from typing import Callable
+import pytest
+from probnode import N, P
 from probnode.computation.expand import expand
+from probnode.probability.event import SureEvent
 
-from probnode.probability.probability import SimpleProbabilityExpression
+from probnode.probability.probability import *
 
 
-def test_expand_simple_prob_exp(
-    simple_prob_expression: Callable[[str], SimpleProbabilityExpression]
+def test_expand_simple_prob_exp(simple_prob_expression1: SimpleProbabilityExpression):
+  assert expand(N(simple_prob_expression1)) == N(simple_prob_expression1)
+
+
+def test_expand_invert_prob_exp(
+    simple_prob_expression1: SimpleProbabilityExpression,
+    simple_invert_prob_expression1: SimpleInvertProbabilityExpression
     ):
-  p1 = simple_prob_expression("Event 1")
-  p2 = simple_prob_expression("Event 1")
-  assert expand(p1) == p2
+  assert expand(N(simple_invert_prob_expression1)
+                ) == (N(P(SureEvent())) - N(simple_prob_expression1))
+
+
+def test_expand_and_prob_exp(
+    simple_prob_expression1: SimpleProbabilityExpression,
+    simple_prob_expression2: SimpleProbabilityExpression,
+    and_prob_expression: AndProbabilityExpression,
+    ):
+  assert expand(N(and_prob_expression)) == (N(simple_prob_expression1) * N(simple_prob_expression2))
+
+
+def test_expand_or_prob_exp(
+    simple_prob_expression1: SimpleProbabilityExpression,
+    simple_prob_expression2: SimpleProbabilityExpression,
+    or_prob_expression: OrProbabilityExpression,
+    and_prob_expression: AndProbabilityExpression,
+    ):
+  assert expand(
+      N(or_prob_expression)
+      ) == (N(simple_prob_expression1) + N(simple_prob_expression2) - N(and_prob_expression))
+
+
+def test_expand_conditional_prob_exp(
+    simple_prob_expression1: SimpleProbabilityExpression,
+    simple_prob_expression2: SimpleProbabilityExpression,
+    or_prob_expression: OrProbabilityExpression, and_prob_expression: AndProbabilityExpression,
+    conditional_prob_expression: ConditionalProbabilityExpression
+    ):
+  assert expand(N(conditional_prob_expression)
+                ) == (N(and_prob_expression) / N(simple_prob_expression2))
