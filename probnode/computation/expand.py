062fb23bdc9f4f999561766d85dc8117111ce7fd
Luong Duc <vinhduc91@outlook.com>
2022-02-12 00:40:19 +0700

    expand to list of possible expression instead of fixed result

--- probnode/computation/expand.py
+++ probnode/computation/expand.py
@@ -19,32 +19,47 @@ def expand_pure_node(node: Node, exhausting: bool = False) -> List[Node]:
 def expand_derived_node(node: DerivedNode, exhausting: bool = False) -> List[Node]:
   if type(node) is AdditiveInverseNode:
     expanded_base = expand_pure_node(node.base, exhausting)
-    return AdditiveInverseNode.from_node(expanded_base)
+    return [AdditiveInverseNode.from_node(expanded_base)]
   if type(node) is ReciprocalNode:
     expanded_base = expand_pure_node(node.base, exhausting)
-    return ReciprocalNode.from_node(expanded_base)
-  return node
+    return [ReciprocalNode.from_node(expanded_base)]
+  return [node]
 
 
 def expand_derived_chain_node(node: ChainNode, exhausting: bool = False) -> List[ChainNode]:
   if type(node) is AdditiveInverseChainNode:
     expanded_base = expand_chain_node(node.base, exhausting)
-    return AdditiveInverseChainNode.from_node(expanded_base)
+    return [AdditiveInverseChainNode.from_node(expanded_base)]
   if type(node) is ReciprocalChainNode:
     expanded_base = expand_chain_node(node.base, exhausting)
-    return ReciprocalChainNode.from_node(expanded_base)
-  return node
+    return [ReciprocalChainNode.from_node(expanded_base)]
+  return [node]
 
 
 def expand_chain_node(chain_node: ChainNode, exhausting: bool = False) -> List[ChainNode]:
+  aggregated_possible_chains = []
   for idx, node in enumerate(chain_node.args):
     if issubclass(type(node), ChainNode):
       if issubclass(type(node), DerivedNode):
-        chain_node.args[idx] = expand_derived_chain_node(node)
+        aggregated_possible_chains.append(expand_derived_chain_node(node))
       else:
-        chain_node.args[idx] = expand_chain_node(node)
+        aggregated_possible_chains.append(expand_chain_node(node))
     elif issubclass(type(node), DerivedNode):
-      chain_node.args[idx] = expand_derived_node(node)
+      aggregated_possible_chains.append(expand_derived_node(node))
     else:
-      chain_node.args[idx] = expand_pure_node(node)
-  return chain_node
+      aggregated_possible_chains.append(expand_pure_node(node))
+  possible_chains = get_alternatives_from_list_of_possible_items(aggregated_possible_chains)
+  return possible_chains
+
+
+def get_alternatives_from_list_of_possible_items(possible_list: List[List[object]]):
+  if len(possible_list) > 1:
+    last_items = possible_list.pop()
+    last_alternatives = get_alternatives_from_list_of_possible_items(possible_list)
+    possible_chains = []
+    for alternative in last_alternatives:
+      for item in last_items:
+        possible_chains.append(alternative + [item])
+    return possible_chains
+  elif len(possible_list) == 1:
+    return list(map(lambda x: [x], possible_list[0]))
--- sample/sample.py
+++ sample/sample.py
@@ -1,4 +1,4 @@
-from probnode.computation.expand import expand
+from probnode.computation.expand import *
 from probnode import Event
 from probnode import P
 from probnode import N
@@ -7,26 +7,33 @@ e = Event("Rain")
 p = P(e)
 e1 = Event("Hot")
 p1 = P(e1)
+e2 = Event("Es")
+p6 = P(e2)
+e3 = Event("Mn")
+p7 = P(e3)
 p2 = P(p1 | p)
 p3 = p.invert()
 p4 = P(p3 // p2)
 
 p5 = p2.invert()
-p6 = p5.invert()
-print(repr(p2))
-n = expand(N(p2))
-print(repr(n))
-print(repr(p4))
-print(repr(p5))
-print(repr(p6))
+#p6 = p5.invert()
+# print(repr(p2))
+# n = expand(N(p2))
+# print(repr(n))
+# print(repr(p4))
+# print(repr(p5))
+# print(repr(p6))
 
 print("\n")
-chain = N(p5) + N(p) - N(p1) * N(p3)
+chain = N(p2) + N(p6) - N(p6) * N(p7)
 print(repr(chain))
 ex_chain = expand(chain)
-print(repr(ex_chain))
+for item in ex_chain:
+  print(f"\n {repr(item)}")
 print("\n")
 
-print(repr(N(p3)))
-n = expand(N(p3))
-print(repr(n))
+# print(repr(N(p3)))
+# n = expand(N(p3))
+# print(repr(n))
+
+     #print(get_alternatives_from_list_of_possible_items([[1, 2], [3, 4], [5, 6, 7]]))
