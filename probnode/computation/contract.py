50dc72ecb3ae4183a892cfd67a78bf2caaca9d38
Luong Duc <vinhduc91@outlook.com>
2022-02-16 23:41:02 +0700

    add functions

--- probnode/computation/contract.py
+++ probnode/computation/contract.py
@@ -1,3 +1,4 @@
+from typing import Type, Union
 from probnode.computation.node import *
 
 
@@ -5,19 +6,21 @@ def contract(chain: ChainNode) -> Node:
   if not issubclass(type(chain), ChainNode):
     raise TypeError(f"Chain argument must be subclass of type {ChainNode.__name__}")
 
-  if len(chain.args) == 1:
-    return chain
-  elif len(chain.args) == 2:
-    return contract_2_nodes(chain)
-  elif len(chain.args) == 3:
-    return contract_3_nodes(chain)
-  else:
-    pass
+  return contract_arbitrary_node_group(type(chain), chain.args)
 
 
-def contract_2_nodes(chain: ChainNode):
+def contract_arbitrary_node_group(
+    chain_type: Union[Type[SumNode], Type[ProductNode]], node_list: List[Node]
+    ) -> List[Node]:
+  if chain_type is SumNode:
+    contract_arbitrary_sum_node_group(node_list)
+  if chain_type is ProductNode:
+    contract_arbitrary_product_node_group(node_list)
+
+
+def contract_arbitrary_sum_node_group(node_list: List[Node]) -> List[Node]:
   pass
 
 
-def contract_3_nodes(chain: ChainNode):
+def contract_arbitrary_product_node_group(node_list: List[Node]) -> List[Node]:
   pass
