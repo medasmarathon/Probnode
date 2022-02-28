a96a7deada22be397cd356b9fed584743ddbd630
Duc Luong <vinhduc91@outlook.com>
2022-02-28 22:32:23 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,10 @@ setup(
         },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3-Clause License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"
         ],
     packages=find_packages(exclude=['example*', '*constant*', 'sample*', '*test*']),
     python_requires=">=3.6",
