688ab8254ea02fab36e0b93946cebb89d124c3aa
Luong Duc <vinhduc91@outlook.com>
2022-04-26 00:10:54 +0700

    up 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="probnode",
-    version="0.1.3",
+    version="0.1.4",
     author="Duc Dang",
     author_email="vinhduc91@outlook.com",
     description="Probability expression library",
