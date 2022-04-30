d238024da8afcf0e95cf3f2b204209c562375148
Duc Luong <vinhduc91@outlook.com>
2022-04-30 16:58:37 +0700

    add density function connecting logics

--- probnode/datatype/probability_density_function.py
+++ probnode/datatype/probability_density_function.py
@@ -16,3 +16,38 @@ class ProbabilityDensityFunction:
           )
 
     return False
+
+  def __add__(self, other: "ProbabilityDensityFunction") -> "ProbabilityDensityFunction":
+
+    def sum_f(x: float) -> float:
+      return self(x) + other(x)
+
+    return ProbabilityDensityFunction(sum_f)
+
+  def __sub__(self, other: "ProbabilityDensityFunction") -> "ProbabilityDensityFunction":
+
+    def subtract_f(x: float) -> float:
+      return self(x) - other(x)
+
+    return ProbabilityDensityFunction(subtract_f)
+
+  def __mul__(self, other: "ProbabilityDensityFunction") -> "ProbabilityDensityFunction":
+
+    def multiply_f(x: float) -> float:
+      return self(x) * other(x)
+
+    return ProbabilityDensityFunction(multiply_f)
+
+  def __truediv__(self, other: "ProbabilityDensityFunction") -> "ProbabilityDensityFunction":
+
+    def divide_f(x: float) -> float:
+      return self(x) / other(x)
+
+    return ProbabilityDensityFunction(divide_f)
+
+  def __neg__(self) -> "ProbabilityDensityFunction":
+
+    def negative_f(x: float) -> float:
+      return 0 - self(x)
+
+    return negative_f
