79fb741f581a8d12fea7ac5b16c6ddfcaf916eda
Duc Luong <vinhduc91@outlook.com>
2022-05-19 14:13:38 +0700

    rename sample variables

--- sample/sample.py
+++ sample/sample.py
@@ -1,31 +1,33 @@
 from probnode import Outcome, E
 from probnode.probability.outcome import SureEvent
 
-e1 = Outcome("Sample event 1")
-p1 = E(e1)
-e2 = Outcome("Sample event 2")
-p2 = E(e2)
+outcome1 = Outcome("Sample event 1")
+outcome1_event = E(outcome1)
+outcome2 = Outcome("Sample event 2")
+outcome2_event = E(outcome2)
 
-p3 = E(p1 | p2)     # Or Probability expression
-p4 = E(p1 & p2)     # And Probability expression
-p5 = E(p1 // p2)     # Conditional Probability expression
+outcome_1_or_2_event = E(outcome1_event | outcome2_event)     # Or Probability expression
+outcome_1_and_2_event = E(outcome1_event & outcome2_event)     # And Probability expression
+outcome_1_when_2_event = E(
+    outcome1_event // outcome2_event
+    )     # Conditional Probability expression
 
 # Display mathematical representation of the object(s)
-print(repr(p1))
+print(repr(outcome1_event))
 
 # First convert probability expression to math node
 from probnode import N
 
-n1 = N(p1)     # Each below is a (math) node
-n2 = N(p2)
-n3 = N(p3)
-n4 = N(p4)
-n5 = N(p5)
+n1 = N(outcome1_event)     # Each below is a (math) node
+n2 = N(outcome2_event)
+n_1_or_2 = N(outcome_1_or_2_event)
+n_1_and_2 = N(outcome_1_and_2_event)
+n_1_when_2 = N(outcome_1_when_2_event)
 
 # A sum of nodes
-snode = 2.0 + n1 + n2 - 1 - n4 + N(E(SureEvent()))
+snode = 2.0 + n1 + n2 - 1 - n_1_and_2 + N(E(SureEvent()))
 # A product of nodes
-pnode = n5 * n2
+pnode = n_1_when_2 * n2
 
 from probnode.computation import contract
 
@@ -36,6 +38,6 @@ print(repr(c2))
 
 from probnode.computation import expand
 
-x = expand(n3)
+x = expand(n_1_or_2)
 print(repr(x[0]))
 print(repr(snode))
